6bd2531bd0535e40923f2462a8f4d1adf2c124d4
luchao <1967768885@qq.com>
2020-11-17 17:06:38 +0800

    init module.

---
+++ rpc-examples/src/main/java/com/rpc/examples/RpcExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.rpc.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcExamplesApplication.class, args);
+    }
+
+}
---
+++ rpc-examples/src/test/java/com/rpc/examples/RpcExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpc.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
